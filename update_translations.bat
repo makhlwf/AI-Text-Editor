4869eb9fa549b4840945d9efb198672d44a05e65
makhlwf <altrhwnyashrf1@gmail.com>
2025-12-07 18:08:48 +0200

    more translations enhancments

--- update_translations.bat
+++ update_translations.bat
@@ -1,9 +1,26 @@
 @echo off
-echo === Extracting messages ===
-pybabel extract -F babel.cfg -o locales/messages.pot .
 
-echo === Updating existing translations ===
-pybabel update -d locales -i locales/messages.pot
+set PROJECT_NAME=AI Text Editor
+set PROJECT_VERSION=0.1beta
+set AUTHOR_NAME=Makhlwf
+set AUTHOR_EMAIL=Altrhwnyashrf1@gmail.com
+
+echo === Extracting messages ===
+pybabel extract ^
+    -F babel.cfg ^
+    -o locales/messages.pot ^
+    --project="%PROJECT_NAME%" ^
+    --version="%PROJECT_VERSION%" ^
+    --copyright-holder="%AUTHOR_NAME%" ^
+    --msgid-bugs-address="%AUTHOR_EMAIL%" ^
+    --last-translator="%AUTHOR_NAME% <%AUTHOR_EMAIL%>" ^
+    .
+
+echo === Updating translations ===
+pybabel update -d locales -i locales/messages.pot --update-header-comment
+
+echo === Compiling ===
+pybabel compile -d locales
 
 echo === Done ===
 pause
